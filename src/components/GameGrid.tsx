be12bebc63b23abae82450202d0e451a21f9585b
Chu Van Gia <vangiachu@gmail.com>
2023-07-11 14:51:18 +0700

    Fix GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
 
 	return (
 		<SimpleGrid
-			columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+			columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
 			padding="10px"
 			spacing={6}
 		>
